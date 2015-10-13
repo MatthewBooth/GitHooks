5bda667bbdc4cfbc764d5275fa302621f91c6c3a
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-13 21:23:06 +0100

    Added install githooks shell script

---
+++ install-custom-githooks.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Automatically adds branch name and time spent to every commit message.
+#
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SOURCE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$SOURCE_DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SOURCE_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+git config --global init.templatedir "$SOURCE_DIR"
