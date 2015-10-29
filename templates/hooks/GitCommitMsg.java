07fd228c83057d07a1ff3f9d4e8bc145078a972e
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-29 14:34:06 +0000

    Changed, if you use an invalid work string, it will match the first part that is valid and return only that

--- templates/hooks/GitCommitMsg.java
+++ templates/hooks/GitCommitMsg.java
@@ -8,7 +8,7 @@ public class GitCommitMsg {
     private String mMessage;
     private String mTimeSpent;
 
-    private String WORK_PATTERN_STRING = "work{1}\\s{1}(([0-9]{1,3}d)?([0-9]{1,3}h)?([0-9]{1,3}m)?\\s)";
+    private String WORK_PATTERN_STRING = "work{1}\\s{1}([0-9]{1,3}d)?([0-9]{1,3}h)?([0-9]{1,3}m)?";
     private String BRANCH_PATTERN_STRING = "[a-zA-Z]*-[0-9]{1,4}";
     private String BRANCH_PREFIX_PATTERN_STRING = "[a-zA-Z]*/";
 
